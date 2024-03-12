79e4d02d532406e278aea36eedd5c863d9ca8be8
Lam Nhan <nhan.lh@raksul.com>
2024-03-12 14:57:31 +0700

    Basic default prebuilder

--- prebuild/lib/index.ts
+++ prebuild/lib/index.ts
@@ -1,22 +1,73 @@
-import {Prebuilder as PrebuilderInterface} from '@tinijs/cli';
-import {TiniConfig} from 'tinijs';
+import {resolve, parse} from 'pathe';
+import {readFile} from 'node:fs/promises';
+import {ensureDir, outputFile, copy} from 'fs-extra/esm';
+import {TiniConfig, Prebuilder} from 'tinijs';
+import {listDir, cleanDir} from '@tinijs/cli';
 
-export default function (tiniConfig: TiniConfig) {
-  return new Prebuilder(tiniConfig);
+export interface PrebuildOptions {
+  skipMinifyHtmlLiterals?: boolean;
+  precompileGeneric?: 'none' | 'lite' | 'full';
 }
 
-export class Prebuilder implements PrebuilderInterface {
-  constructor(private tiniConfig: TiniConfig) {}
+export default function (options: PrebuildOptions = {}) {
+  return function (tiniConfig: TiniConfig) {
+    return new DefaultPrebuilder(options, tiniConfig);
+  };
+}
 
-  async buildAll() {
-    console.log('Building all files');
+export class DefaultPrebuilder implements Prebuilder {
+  constructor(
+    private options: PrebuildOptions,
+    private tiniConfig: TiniConfig
+  ) {}
+
+  async build() {
+    const srcPath = resolve(this.tiniConfig.srcDir);
+    await cleanDir(this.tiniConfig.tempDir);
+    const paths = await listDir(srcPath);
+    for (let i = 0; i < paths.length; i++) {
+      await this.buildFile(paths[i]);
+    }
   }
 
   async buildFile(path: string) {
-    console.log('Building file', path);
+    const {dir, base, ext} = parse(path);
+    const innerFilePath = path
+      .split(`/${this.tiniConfig.srcDir}/`)
+      .pop() as string;
+    const outFilePath = resolve(this.tiniConfig.tempDir, innerFilePath);
+
+    // create dir
+    await ensureDir(dir);
+
+    /*
+     * app.html -> index.html
+     */
+    if (base === 'app.html') {
+      await outputFile(
+        outFilePath.replace('/app.html', '/index.html'),
+        await readFile(path, 'utf8')
+      );
+    } else if (ext === '.ts') {
+      /*
+       * copy but skip public dir
+       */
+      const code = await readFile(path, 'utf8');
+      // TODO: prebuild code
+      await outputFile(outFilePath, code);
+    } else if (!this.isUnder(path, 'public')) {
+      /*
+       * copy but skip public dir
+       */
+      await copy(path, outFilePath);
+    }
   }
 
-  async removeFile(path: string) {
-    console.log('Removing file', path);
+  private isUnder(path: string, topDir: string) {
+    return ~path.indexOf(
+      `/${this.tiniConfig.srcDir}/${
+        (this.tiniConfig.dirs as any)?.[topDir] || topDir
+      }/`
+    );
   }
 }
