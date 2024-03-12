f1acead7308e8350b2d38d8c97edf8a49fba533f
Lam Nhan <nhan.lh@raksul.com>
2024-03-12 15:13:52 +0700

    Basic Parcel builder

--- parcel/lib/index.ts
+++ parcel/lib/index.ts
@@ -1,1 +1,47 @@
-export default function () {}
+import {TiniConfig, Builder} from 'tinijs';
+import {getIndexHtmlPath} from '@tinijs/cli';
+
+export interface BuildOptions {
+  buildCommand?: string;
+  devCommand?: string;
+  onDevServerStart?: () => void;
+}
+
+export default function (options: BuildOptions = {}) {
+  return function (tiniConfig: TiniConfig) {
+    return new ParcelBuilder(options, tiniConfig);
+  };
+}
+
+export class ParcelBuilder implements Builder {
+  constructor(
+    private options: BuildOptions,
+    private tiniConfig: TiniConfig
+  ) {}
+
+  get build() {
+    return {
+      command: this.options.buildCommand || this.commands.buildCommand,
+    };
+  }
+
+  get dev() {
+    return {
+      command: this.options.devCommand || this.commands.devCommand,
+      onServerStart: this.options.onDevServerStart,
+    };
+  }
+
+  private get commands() {
+    const {outDir} = this.tiniConfig;
+    const indexHtmlPath = getIndexHtmlPath(this.tiniConfig);
+    return {
+      buildCommand:
+        this.options.buildCommand ||
+        `parcel build "${indexHtmlPath}" --dist-dir ${outDir} --no-cache`,
+      devCommand:
+        this.options.devCommand ||
+        `parcel "${indexHtmlPath}" --dist-dir ${outDir} --port 3000 --no-cache --log-level none`,
+    };
+  }
+}
